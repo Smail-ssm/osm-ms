2d374f31975c4fc2576d29af0ff51becce5070b8
Smail <ismailmansouri571@gmail.com>
2025-04-19 16:32:07 +0100

    adding bank account managment and compony profile

---
+++ oilproductionservice/src/main/java/com/osm/oilproductionservice/enums/Currency.java
@@ -0,0 +1,7 @@
+package com.osm.oilproductionservice.enums;
+
+public enum Currency {
+    TND,
+    EUR,
+    USD
+}
---
+++ oilproductionservice/src/main/java/com/osm/oilproductionservice/repository/BankAccountRepository.java
@@ -0,0 +1,7 @@
+package com.osm.oilproductionservice.repository;
+
+import com.osm.oilproductionservice.model.BankAccount;
+import com.xdev.xdevbase.repos.BaseRepository;
+
+public interface BankAccountRepository extends BaseRepository<BankAccount> {
+}
